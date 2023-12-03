790b9b5fd5402b6c23f44e822bdfe50180e7eccc
Enric <qbreis@gmail.com>
2023-12-03 12:47:05 +0100

    chore: initial commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+// use League\CommonMark\CommonMarkConverter;
+
+//$converter = new CommonMarkConverter();
+
+// To use Environment with Extensions
+use League\CommonMark\Environment\Environment;
+
+// To use Extensions
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+
+// Front Matter Extension
+use League\CommonMark\Extension\FrontMatter\FrontMatterExtension;
+use League\CommonMark\Extension\FrontMatter\Output\RenderedContentWithFrontMatter;
+
+// External Links Extension
+use League\CommonMark\Extension\ExternalLink\ExternalLinkExtension;
+
+// Highlight Extension
+use League\CommonMark\Extension\CommonMark\Node\Block\FencedCode;
+use League\CommonMark\Extension\CommonMark\Node\Block\IndentedCode;
+use Spatie\CommonMarkHighlighter\FencedCodeRenderer;
+use Spatie\CommonMarkHighlighter\IndentedCodeRenderer;
+
+use League\CommonMark\MarkdownConverter;
+
+// Define your configuration, if needed
+$config = [
+    'html_input' => 'escape', // How to handle raw HTML. 'strip' will ignore all html tags not allowed, while 'escape' will allow but escape them.
+    'allow_unsafe_links' => false, // Whether unsafe links are permitted.
+
+    'external_link' => [
+        'internal_hosts' => 'localhost', // TODO: Don't forget to set this!
+        'open_in_new_window' => true,
+        'html_class' => 'external-link',
+        'nofollow' => '',
+        'noopener' => 'external',
+        'noreferrer' => 'external',
+    ],
+];
+
+// Configure the Environment with all the CommonMark parsers/renderers.
+$environment = new Environment($config);
+$environment->addExtension(new CommonMarkCoreExtension());
+
+// Add the extension
+$environment->addExtension(new FrontMatterExtension());
+
+// Add External Links Extension
+$environment->addExtension(new ExternalLinkExtension());
+
+// Add Highlight Renderer
+$environment->addRenderer(FencedCode::class, new FencedCodeRenderer(['html', 'php', 'js']));
+$environment->addRenderer(IndentedCode::class, new IndentedCodeRenderer(['html', 'php', 'js']));
+
+// Instantiate the converter engine and start converting some Markdown!
+$converter = new MarkdownConverter($environment);
+
+$markdown = file_get_contents('posts/hola-world.md');
+
+$markdownConverted = $converter->convert($markdown);
+
+$frontMatter = $markdownConverted instanceof RenderedContentWithFrontMatter
+    ?
+    $markdownConverted->getFrontMatter()
+    :
+    '';
+
+echo $markdownConverted;
+
+echo '<pre>';
+print_r($frontMatter);
+echo '</pre>';
+?>
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/styles/panda-syntax-dark.min.css">
---
+++ posts/hola-world.md
@@ -0,0 +1,32 @@
+---
+layout: post
+title: I Love Markdown
+tags:
+  - test
+  - example
+---
+# Hola World!
+*Lead content.*
+
+<strong>*Hard coded* strong HTML element inside MD!</strong>
+
+[local link](posts/hola-world.md)
+
+[Dillinger Markdown Editor](https://dillinger.io)
+
+![Alternative text for image](https://blog-qbreis.vercel.app/images/logo-github.svg)
+*I want some optional caption for some of my pictures.*
+
+```js{1,4-5} Do not leave spaces after commas and hyphens
+// Love at first sight
+if (me.getDistanceTo(you.position) < 200) {
+  me.setFeelings({
+    inLove: true,
+  });
+}
+```
+*Source code by [Learning To Code By Writing Code Poems — Smashing Magazine](https://www.smashingmagazine.com/2018/07/writing-code-poems/)*
+
+>   This is blockquote eleemnt
+
+I successfully installed the <https://github.com/thephpleague/commonmark> project!
